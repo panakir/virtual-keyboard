e184da787ecd1cbb50fa7b113d2de83b2f809e48
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-04-30 16:09:35 +0300

    feat: implement Key class (Sun, April 30, 2023 04:09PM)

---
+++ src/scripts/Key.js
@@ -0,0 +1,23 @@
+class Key {
+  constructor(value, code, width, addition) {
+    this.value = value
+    this.code = code
+    this.width = width
+    this.addition = addition
+  }
+
+  generateKey() {
+    const key = document.createElement('button')
+    key.classList.add('key')
+    key.textContent = this.value
+    key.dataset.code = this.code
+    key.dataset.addition = this.cadditionde
+    if (this.width > 1) {
+      key.classList.add(`width-${this.width}`)
+    }
+    
+    return key
+  }
+}
+
+export default Key
