c9ea27d90ab3a534d1f565e421d03728ebbf4bdd
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-05-01 10:55:41 +0300

    feat: add import mova from _languge.js, add currentLanguage in Keyboard`s constructor, rewrite generateKeys, printKey methods, add backspaceHandler, deleteHandler, capslockHandler methods (Mon, May 1, 2023 10:55AM)

--- src/scripts/Keyboard.js
+++ src/scripts/Keyboard.js
@@ -1,4 +1,4 @@
-import { english } from "./_language.js";
+import { english, mova } from "./_language.js";
 import Key from "./Key.js";
 
 class Keyboard {
@@ -6,16 +6,15 @@ class Keyboard {
     this.language = language
     this.area = area
     this.functionalKeyCode = ['Backspace', 'Delete','CapsLock', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'ControlRight']
+    this.currentLanguage = language
     this.pressed = new Set()
   }
 
-
   generateKeys() {
-    if (this.language === 'en') {
-      this.keys = english.map((keys) => new Key(...keys).generateKey())
-    }
     if (this.language === 'by') {
-      this.keys = belarus.map((keys) => new Key(...keys).generateKey())
+      this.keys = mova.map((key) => new Key(...key).generateKey())
+    } else {
+      this.keys = english.map((key) => new Key(...key).generateKey())
     }
   }
 
@@ -58,9 +57,41 @@ class Keyboard {
 
   printKey(key) {
     const position = this.area.selectionStart
-    this.area.value += key.textContent
-    this.area.setSelectionRange(position + 1, position + 1)
+    if (key.dataset.code === 'Enter') {
+      this.area.value = this.area.value.slice(0, position) + '\n' + this.area.value.slice(position, this.area.value.length)
+    } else if (key.dataset.code === 'Tab') {
+      this.area.value = this.area.value.slice(0, position) + '\t' + this.area.value.slice(position, this.area.value.length)
+    } else {
+      this.area.value = this.area.value.slice(0, position) + key.textContent + this.area.value.slice(position, this.area.value.length)
+      this.area.setSelectionRange(position + 1, position + 1)
+    }
   }
-}
 
+  backspaceHandler(position) {
+    if (position > 0) {
+      this.area.value = this.area.value.slice(0, position - 1) + this.area.value.slice(position, this.area.value.length)
+      this.area.setSelectionRange(position - 1, position - 1)
+    }
+  }
+
+  deleteHandler(position) {
+    this.area.value = this.area.value.slice(0, position) + this.area.value.slice(position + 1, this.area.value.length)
+    this.area.setSelectionRange(position, position)
+  }
+
+  capslockHandler(key) {
+    console.log('capsed');
+    key.classList.toggle('press')
+    this.keys.forEach(key => {
+      if (key.dataset.code.includes('Key') || 'ёхъжэбю'.includes(key.textContent.toLowerCase())) {
+        if (key.textContent === key.textContent.toLowerCase()) {
+          key.textContent = key.textContent.toUpperCase()
+        } else {
+          key.textContent = key.textContent.toLowerCase()
+        }
+      }
+    })
+  }
+  
+}
 export default Keyboard
